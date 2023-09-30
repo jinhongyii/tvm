0c30cc3c6e9c37986db149c49e9c7b8e1ea097b2
Hongyi Jin <jinhongyi02@gmail.com>
2023-09-29 20:30:20 -0400

    update tests on mlp and attention

--- tests/python/relax/distributed/test_distributed_distir_disco_integration.py
+++ tests/python/relax/distributed/test_distributed_distir_disco_integration.py
@@ -33,6 +33,9 @@ from tvm.script.parser import tir as T
 from tvm import get_global_func
 from tvm import relax
 
+_all_session_kinds = [di.ThreadedSession, di.ProcessSession]
+_ccl = [get_global_func("runtime.disco.compiled_ccl")()]
+
 def create_device_target(ccl):
     if ccl == "nccl":
         dev = tvm.cuda(0)
@@ -41,6 +44,8 @@ def create_device_target(ccl):
     target = tvm.target.Target.from_device(dev)
     return (dev, target)
 
+@pytest.mark.parametrize("session_kind", _all_session_kinds)
+@pytest.mark.parametrize("ccl", _ccl)
 def test_mlp(session_kind, ccl):  # pylint: disable=too-many-locals
     devices = [0, 1]
     sess = session_kind(num_workers=len(devices))
@@ -137,5 +142,161 @@ def test_mlp(session_kind, ccl):  # pylint: disable=too-many-locals
     # pylint: enable=invalid-name
     np.testing.assert_allclose(Y_result, Y_expected, rtol=1e-4, atol=1e-4)
     
+@pytest.mark.parametrize("session_kind", _all_session_kinds)
+@pytest.mark.parametrize("ccl", _ccl)
+def test_attention(session_kind, ccl):  # pylint: disable=too-many-locals,too-many-statements
+    devices = [0, 1]
+    sess = session_kind(num_workers=len(devices))
+    sess.init_ccl(ccl, *devices)
+
+    # pylint: disable=invalid-name
+    @tvm.script.ir_module
+    class Attention:  # pylint: disable=too-few-public-methods
+        @R.function
+        def main(  # pylint: disable=too-many-locals
+            x: R.Tensor((1, 10, 128), "float32"),
+            Wq: R.Tensor((128, 512), "float32"),
+            Wk: R.Tensor((128, 512), "float32"),
+            Wv: R.Tensor((128, 512), "float32"),
+            Wo: R.Tensor((512, 128), "float32"),
+        ) -> R.Tensor((128, 128), "float32"):
+            R.func_attr({"global_symbol": "main"})
+            with R.dataflow():
+                # q
+                lv0: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wq)
+                lv1: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv0, [1, 10, 8, 64])
+                lv2: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv1, [0, 2, 1, 3])
+                # k
+                lv3: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wk)
+                lv4: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv3, [1, 10, 8, 64])
+                lv5: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv4, [0, 2, 1, 3])
+                # v
+                lv6: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wv)
+                lv7: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv6, [1, 10, 8, 64])
+                lv8: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv7, [0, 2, 1, 3])
+                # softmax(q @ k / sqrt(dk))
+                lv9: R.Tensor((1, 8, 64, 10), "float32") = R.permute_dims(lv5, [0, 1, 3, 2])
+                lv10: R.Tensor((1, 8, 10, 10), "float32") = R.matmul(lv2, lv9)
+                lv11: R.Tensor((1, 8, 10, 10), "float32") = R.multiply(
+                    lv10, R.const(1 / 8, "float32")
+                )
+                lv12: R.Tensor((1, 8, 10, 10), "float32") = R.nn.softmax(lv11, axis=-1)
+                # attn_weight @ v
+                lv13: R.Tensor((1, 8, 10, 64), "float32") = R.matmul(lv12, lv8)
+                lv14: R.Tensor((1, 10, 8, 64), "float32") = R.permute_dims(lv13, [0, 2, 1, 3])
+                lv15: R.Tensor((1, 10, 512), "float32") = R.reshape(lv14, [1, 10, 512])
+                # attn_output @ o
+                lv16: R.Tensor((1, 10, 128), "float32") = R.matmul(lv15, Wo)
+                R.output(lv16)
+            return lv16
+    @tvm.script.ir_module
+    class AnnotatedAttention:  # pylint: disable=too-few-public-methods
+        I.module_attrs({"device_num": 2})
+        I.module_global_infos({"mesh": [R.device_mesh((2,), I.Range(0, 2))]})
+        @R.function
+        def main(  # pylint: disable=too-many-locals
+            x: R.Tensor((1, 10, 128), "float32"),
+            Wq: R.Tensor((128, 512), "float32"),
+            Wk: R.Tensor((128, 512), "float32"),
+            Wv: R.Tensor((128, 512), "float32"),
+            Wo: R.Tensor((512, 128), "float32"),
+        ) -> R.Tensor((128, 128), "float32"):
+            R.func_attr({"global_symbol": "main", "num_input": 1})
+            with R.dataflow():
+                # q
+                lv0: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wq)
+                lv1: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv0, [1, 10, 8, 64])
+                lv1 = R.dist.annotate_sharding(lv1, "mesh[0]", "S[2]")
+                lv2: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv1, [0, 2, 1, 3])
+                # k
+                lv3: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wk)
+                lv4: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv3, [1, 10, 8, 64])
+                lv4 = R.dist.annotate_sharding(lv4, "mesh[0]", "S[2]")
+                lv5: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv4, [0, 2, 1, 3])
+                # v
+                lv6: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wv)
+                lv7: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv6, [1, 10, 8, 64])
+                lv7 = R.dist.annotate_sharding(lv7, "mesh[0]", "S[2]")
+                lv8: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv7, [0, 2, 1, 3])
+                # softmax(q @ k / sqrt(dk))
+                lv9: R.Tensor((1, 8, 64, 10), "float32") = R.permute_dims(lv5, [0, 1, 3, 2])
+                lv10: R.Tensor((1, 8, 10, 10), "float32") = R.matmul(lv2, lv9)
+                lv11: R.Tensor((1, 8, 10, 10), "float32") = R.multiply(
+                    lv10, R.const(1 / 8, "float32")
+                )
+                lv12: R.Tensor((1, 8, 10, 10), "float32") = R.nn.softmax(lv11, axis=-1)
+                # attn_weight @ v
+                lv13: R.Tensor((1, 8, 10, 64), "float32") = R.matmul(lv12, lv8)
+                lv14: R.Tensor((1, 10, 8, 64), "float32") = R.permute_dims(lv13, [0, 2, 1, 3])
+                lv15: R.Tensor((1, 10, 512), "float32") = R.reshape(lv14, [1, 10, 512])
+                # attn_output @ o
+                lv16: R.Tensor((1, 10, 128), "float32") = R.matmul(lv15, Wo)
+                R.output(lv16)
+            return lv16
+
+    # pylint: enable=invalid-name
+    dev, target = create_device_target(ccl)
+
+    def relax_build(mod, target):
+        with target:
+            mod = rx.get_pipeline("zero")(mod)  # pylint: disable=no-value-for-parameter
+            mod = dl.ApplyDefaultSchedule(  # pylint: disable=not-callable
+                dl.gpu.Matmul(),
+                dl.gpu.GEMV(),
+                dl.gpu.Reduction(),
+                dl.gpu.GeneralReduction(),
+                dl.gpu.Fallback(),
+            )(mod)
+            return rx.build(mod, target=target)
+
+    # pylint: disable=invalid-name
+    X = np.random.randn(1, 10, 128).astype("float32")
+    Wq = np.random.randn(128, 512).astype("float32")
+    Wk = np.random.randn(128, 512).astype("float32")
+    Wv = np.random.randn(128, 512).astype("float32")
+    Wo = np.random.randn(512, 128).astype("float32")
+    Y_expected = VirtualMachine(relax_build(Attention, target), device=dev)["main"](
+        tvm.nd.array(X, device=dev),
+        tvm.nd.array(Wq, device=dev),
+        tvm.nd.array(Wk, device=dev),
+        tvm.nd.array(Wv, device=dev),
+        tvm.nd.array(Wo, device=dev),
+    ).numpy()
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = tmpdir + "/test.so"
+        sharded_mod = AnnotatedAttention
+        sharded_mod = relax.transform.LegalizeOps({"relax.reshape": (lambda bb, call: call)})(sharded_mod)
+        sharded_mod = relax.distributed.transform.PropagateSharding()(sharded_mod)
+        sharded_mod = relax.distributed.transform.LowerGlobalViewToLocalView()(sharded_mod)
+        sharded_mod = relax.distributed.transform.LowerDistIR()(sharded_mod)
+        print(sharded_mod)
+        relax_build(sharded_mod, target).export_library(path)
+
+        mod = sess.load_vm_module(path)
+
+        d_X = sess.empty((1, 10, 128), "float32")
+        d_Wq = sess.empty((128, 256), "float32")
+        d_Wk = sess.empty((128, 256), "float32")
+        d_Wv = sess.empty((128, 256), "float32")
+        d_Wo = sess.empty((256, 128), "float32")
+
+        d_X.debug_copy_from(0, X)
+        d_Wq.debug_copy_from(0, Wq[:, :256])
+        d_Wq.debug_copy_from(1, Wq[:, 256:])
+        d_Wk.debug_copy_from(0, Wk[:, :256])
+        d_Wk.debug_copy_from(1, Wk[:, 256:])
+        d_Wv.debug_copy_from(0, Wv[:, :256])
+        d_Wv.debug_copy_from(1, Wv[:, 256:])
+        d_Wo.debug_copy_from(0, Wo[:256, :])
+        d_Wo.debug_copy_from(1, Wo[256:, :])
+        d_Y = mod["main"](d_X, d_Wq, d_Wk, d_Wv, d_Wo)
+        Y_result = tvm.nd.empty((1, 10, 128), "float32", device=dev)
+        sess.copy_from_worker_0(Y_result, d_Y)
+        sess.sync_worker_0()
+        Y_result = Y_result.numpy()
+    # pylint: enable=invalid-name
+    np.testing.assert_allclose(Y_result, Y_expected, rtol=1e-3, atol=1e-3)
+    
 if __name__ == "__main__":
-    test_mlp(di.ProcessSession, "rccl" )
+    tvm.testing.main()
