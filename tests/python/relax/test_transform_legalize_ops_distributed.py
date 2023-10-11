7133e37556963ac52ffaf20e18c3191cfd73e6e0
Hongyi Jin <jinhongyi02@gmail.com>
2023-10-10 20:26:20 -0400

    update test

--- tests/python/relax/test_transform_legalize_ops_distributed.py
+++ tests/python/relax/test_transform_legalize_ops_distributed.py
@@ -21,7 +21,7 @@ from tvm import relax
 from tvm.relax.transform import LegalizeOps
 from tvm.script import ir as I
 from tvm.script import relax as R
-
+from tvm.script import tir as T
 def test_redistribute_replica_to_shard():
     # fmt: off
     @tvm.script.ir_module
@@ -33,20 +33,30 @@ def test_redistribute_replica_to_shard():
 
     @I.ir_module
     class Expected:
+        @T.prim_func(private=True)
+        def te_R_to_S(A: T.Buffer((T.int64(10), T.int64(10)), "float32"), redistribute_replica_to_shard: T.Buffer((T.int64(10), T.int64(5)), "float32"), worker_id: T.int64):
+            T.func_attr({"tir.noalias": T.bool(True)})
+            # with T.block("root"):
+            for i0, i1 in T.grid(T.int64(10), T.int64(5)):
+                with T.block("redistribute_replica_to_shard"):
+                    v_i0, v_i1 = T.axis.remap("SS", [i0, i1])
+                    T.reads(A[v_i0, worker_id * T.int64(5) + v_i1])
+                    T.writes(redistribute_replica_to_shard[v_i0, v_i1])
+                    redistribute_replica_to_shard[v_i0, v_i1] = A[v_i0, worker_id * T.int64(5) + v_i1]
+
         @R.function
-        def main(x: R.Tensor((10, 10), dtype="float32")) -> R.Tensor((10, 10), dtype="float32"):
-            gv0: R.Tensor((10, 10), dtype="float32") = R.call_dps_packed("runtime.disco.allreduce", [x, R.shape([0])], out_sinfo=R.Tensor((10, 10), dtype="float32"))
-            gv1: R.Tensor((10, 10), dtype="float32") = R.call_dps_packed("runtime.disco.allreduce", [x, R.shape([1])], out_sinfo=R.Tensor((10, 10), dtype="float32"))
-            gv2: R.Tensor((10, 10), dtype="float32") = R.call_dps_packed("runtime.disco.allreduce", [x, R.shape([2])], out_sinfo=R.Tensor((10, 10), dtype="float32"))
-            gv3: R.Tensor((10, 10), dtype="float32") = R.call_dps_packed("runtime.disco.allreduce", [x, R.shape([3])], out_sinfo=R.Tensor((10, 10), dtype="float32"))
-            gv4: R.Tensor((10, 10), dtype="float32") = R.call_dps_packed("runtime.disco.allreduce", [x, R.shape([4])], out_sinfo=R.Tensor((10, 10), dtype="float32"))
-            return x
+        def main(x: R.Tensor((10, 10), dtype="float32")) -> R.Tensor((10, 5), dtype="float32"):
+            worker_id = T.int64()
+            cls = Expected
+            gv: R.Shape(ndim=-1) = R.call_pure_packed("runtime.disco.worker_id", sinfo_args=(R.Shape(ndim=-1),))
+            gv1: R.Shape([worker_id]) = R.match_cast(gv, R.Shape([worker_id]))
+            gv0 = R.call_tir(cls.te_R_to_S, (x,), out_sinfo=R.Tensor((10, 5), dtype="float32"), tir_vars=R.shape([worker_id]))
+            return gv0
     # fmt: on
 
     mod = LegalizeOps()(Before)
-    print(mod)
-    relax.build(mod, target="llvm")
+    tvm.ir.assert_structural_equal(mod, Expected)
 
 
 if __name__ == "__main__":
-    test_redistribute_replica_to_shard()
+    tvm.testing.main()
